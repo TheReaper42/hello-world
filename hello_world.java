720037e3912942bec7159402acfb8bc725c3eb66
The Reaper <dimonnumber1@gmail.com>
2021-08-16 21:17:15 +0300

    hello world is now hello matesg

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,6 @@
   
 public class AnnoyingOrange {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, matesg!");
     }
 }
